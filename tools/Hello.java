6f2192d03826853b464a7721ef985b6dfbf7da5c
AYOUB YOUB <sis.ayoub.youb@gmail.com>
2024-03-13 13:11:08 +0000

    added

---
+++ tools/Hello.java
@@ -0,0 +1,17 @@
+package tools;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Hello {
+
+  public static void main(String[] args) {
+    List<Integer> nums = Arrays.asList(4, 5, 7, 2);
+    nums
+      .stream()
+      .map(n -> n *2)
+      .filter(n -> n != 8)
+      .sorted()
+      .forEach(n -> System.out.println(n));
+  }
+}
